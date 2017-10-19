7d8b7c37ecbfd385381e0654e33c956277ee38fa
CJ O'Sullivan <me@cjosullivan.net>
2017-10-19 16:33:00 +0100

    Added a jobsGetOne route to the API

--- api/controllers/jobs.controllers.js
+++ api/controllers/jobs.controllers.js
@@ -1,8 +1,17 @@
 var jobData = require('../data/job-data.json');
 
 module.exports.jobsGetAll = function(req,res){
-    console.log("GET the jobs");
+    console.log("GET all jobs");
     res
         .status(200)
         .json(jobData);
+};
+
+module.exports.jobsGetOne = function(req,res){
+    var jobId = req.params.jobId;
+    var thisJob = jobData[jobId];
+    console.log("GET jobId ", jobId);
+    res
+        .status(200)
+        .json(thisJob);
 };
